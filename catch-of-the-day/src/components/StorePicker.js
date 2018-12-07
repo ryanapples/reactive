ef8fe028f4d9366a8d1bbd3ec21a610f7695ad2d
apellanr <ryan.apellanes@gmail.com>
2018-12-06 23:03:19 -0800

    catch of the day components - Header, Order, Inventory

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React from "react";
 
 class StorePicker extends React.Component {
   render() {
-    return(
-      
-    )
+    return (
+      <form className="store-selector">
+        <h2>Please Enter A Store</h2>
+        <input type="text" required placeholder="Store Name" />
+        <button type="submit">Visit Store →</button>
+      </form>
+    );
   }
 }
 
